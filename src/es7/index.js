d95d1e05c2781d7ae62df056e10a1064a87874e6
luis_parra <luis.parra.pulido@gmail.com>
2020-04-06 10:30:03 -0500

    clase 5: ES7 includes y exponencial

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+//includes
+let number = [1, 2, 3, 7, 8]
+
+if (number.includes(7)) {
+    console.log('Si se encuentra el valor de 7')
+} else {
+    console.log('No se encuentra el valor de 7')
+}
+
+//--------------------------------------------------------
+
+//exponente
+let base = 4
+let exp = 3
+let result = base ** exp
+
+console.log(result)
